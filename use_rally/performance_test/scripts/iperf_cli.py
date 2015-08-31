054ee33a1d5e0fb934b2e0f622139ab488829a07
ymhope <ymhope@126.com>
2015-08-31 17:18:02 +0800

    iperf

--- use_rally/performance_test/scripts/iperf_cli.py
+++ use_rally/performance_test/scripts/iperf_cli.py
@@ -7,15 +7,16 @@ import socket
 import fcntl
 import struct
 
-server_ip='192.168.1.122'
+server_ip='127.0.0.1'
 def test1():
-    result=commands.getstatusoutput('iperf -c %s -i 10'%server_ip)
+    result=commands.getstatusoutput('iperf -c %s'%server_ip)
     ip_from=get_ip_address('eth0')
     try:
         myfile=open('./iperf_log.txt','a+')
         if result[0]==0:
             myresult=re.findall('\d*\.\d*\s\w*/sec',result[1])
-            myfile.write('\nfrom'+ip_from+'to'+server_ip+'\tBandwidth->'+myresult[0]+'\t'+time.strftime('%c',time.localtime(time.time())))
+            myresult1='\t'.join(myresult)
+            myfile.write('\nfrom'+ip_from+'to'+server_ip+'\tBandwidth->'+myresult1+'\t'+time.strftime('%c',time.localtime(time.time())))
         else:
             myfile.write('\n'+'ERROR\t'+result[1]+time.strftime('%c',time.localtime(time.time())))  
     finally:
